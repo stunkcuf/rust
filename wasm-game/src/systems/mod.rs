7f1f10024353560e945fba26dddd909dfaaced98
stunkcuf <mychal.bert@gmail.com>
2025-10-24 22:59:19 -0700

    Delete wasm-game/src directory

--- wasm-game/src/systems/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod clouds;
-pub mod particles;
-// Future modules can be added here, for example:
-// pub mod water;
-// pub mod player;
-// pub mod shattered_shores;
-// pub mod mystic_forest;
